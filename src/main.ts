cb00f91f185775d41c54fd79fc4fae46fb089412
AbhiPatelBizLine <abhipatel.bizline@linics.co>
2025-04-24 22:12:07 +0530

    Update: Cors values

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,14 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe()); 
-  
-  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
 
- 
+  app.enableCors({
+    origin: '*', // Allow all origins (you can specify specific domains if needed)
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Specify allowed methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
+  });
+
   await app.listen(process.env.PORT ?? 8080);
 }
 bootstrap();
