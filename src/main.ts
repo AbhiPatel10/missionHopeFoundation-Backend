9629a106669502b9e3b0ea55dfb02059adbcedf2
AbhiPatelBizLine <abhipatel.bizline@linics.co>
2025-04-27 22:57:41 +0530

    Update: vercel and main for CORS

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
 
 
   app.enableCors({
-    origin: '*', // Allow all origins (you can specify specific domains if needed)
+    origin: ['http://localhost:3000', 'https://missionhopefoundation.in'], // Allow all origins (you can specify specific domains if needed)
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Specify allowed methods
     allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
     credentials: true
